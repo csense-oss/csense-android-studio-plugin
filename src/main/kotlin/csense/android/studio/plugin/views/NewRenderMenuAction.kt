82c02fbc171e4916b83a1d7a9539224470b475c8
kasper Tvede <kasper@webmasteren.eu>
2019-01-30 00:16:50 +0100

    and more stuff to come.

---
+++ src/main/kotlin/csense/android/studio/plugin/views/NewRenderMenuAction.kt
@@ -0,0 +1,11 @@
+package csense.android.studio.plugin.views
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class NewRenderMenuAction:  AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+
+}
